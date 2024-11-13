cfa71728a8369ff2c0da62c1a293a3558086980a
Santiago Mattiauda <santimattius@gmail.com>
2024-11-13 19:24:35 -0300

    [feature] add zipline cache

--- composeApp/src/androidMain/kotlin/com/santimattius/kmp/skeleton/di/Dependencies.android.kt
+++ composeApp/src/androidMain/kotlin/com/santimattius/kmp/skeleton/di/Dependencies.android.kt
@@ -1,15 +1,24 @@
 package com.santimattius.kmp.skeleton.di
 
+import android.content.Context
 import com.santimattius.kmp.core.AndroidServerManager
 import com.santimattius.kmp.core.ServerManager
+import com.santimattius.kmp.skeleton.applicationContext
 import kotlinx.coroutines.MainScope
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val platformModule: Module
     get() = module {
+        factory<Context> {
+            val context = applicationContext ?: run {
+                throw IllegalArgumentException("Application context is null")
+            }
+            context
+        }
         factory<ServerManager> {
             AndroidServerManager(
+                context = get(),
                 manifestUrl = "http://10.0.2.2:8080/manifest.zipline.json",
                 scope = MainScope()
             )
